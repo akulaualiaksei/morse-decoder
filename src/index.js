1b5936ed912bc4ea7ffb73b54a0fcdb28695a562
Aliaksei_Akulau <akulaualiaksei@gmail.com>
2020-10-23 21:54:26 +0300

    feat: add morse-decoder task

--- src/index.js
+++ src/index.js
@@ -39,8 +39,35 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+  let lettersArray = expr.match(/.{1,10}/g);
+  let buffer='',bufferResult='',str = '';
+  let result = [];
+    for (let i = 0; i < lettersArray.length; i++) {
+      buffer = `${lettersArray[i]}`;
+    //   console.log(buffer);
+      if (buffer === '**********') {
+          result.push(' ');
+        } else {
+      for (let j=0; j<lettersArray[i].length; j += 2) {
+        str = buffer.slice(0,2);
+        // console.log(str);
+        if (str == '11') {
+          bufferResult = bufferResult + '-';
+        } else if (str == '10') {
+          bufferResult = bufferResult + '.';
+        }
+        buffer = buffer.slice(2);
+        }
+        result.push(MORSE_TABLE[`${bufferResult}`]);
+
+          bufferResult='';
+      }
+    }
+    return result.join("");
+
 }
 
 module.exports = {
     decode
 }
+
